8c7f250f05cd91091afd24405d59cc81025655bd
Jonathan Lee <Jonathan Lee@miyoshi.local>
2022-12-15 16:51:45 +0900

    Fixed regex-related highlighting bug

--- resources/templatetags/resources_tags.py
+++ resources/templatetags/resources_tags.py
@@ -15,21 +15,21 @@ def highlight_query(text, query):
         includes the CSS class 'highlight_query'.
         """
 
-    # It is necessary to escape the text and query strings to avoid erroneous
-    # highlighting in the case of strings that include ampersands, angle
-    # brackets, and other chars that might be interpreted as html.
-    # (See the below comment regarding why the same name is used for the 'text'
-    # string.)
+    # Necessary to escape characters that may be interpreted as html in the text
+    # and query strings to avoid erroneous highlighting.
     text = escape(text)
-    escaped_query = escape(query)
+    html_escaped_query = escape(query)
 
-    matches = re.finditer(escaped_query, text, re.IGNORECASE)
+    # Also necessary to escape regular expression metacharacters in the query.
+    regex_escaped_query = re.escape(html_escaped_query)
+
+    matches = re.finditer(regex_escaped_query, text, re.IGNORECASE)
 
     # In the for loop below, it is necessary to distinguish between the text
     # string in the first loop and that in the second and subsequent loops.
     # This would necessitate an additional if condition and more code.
     # Using the same name for the 'text' string avoids this and keeps the code
-    # cleaner and simpler (except for the extra comment...).
+    # cleaner and simpler (except for the extra long comment...).
     if matches:
         matches_reversed = reversed(list(matches))
         for m in matches_reversed:
