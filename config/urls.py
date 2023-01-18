0f5f40e452d1a6ebbf34805d2ad4ae6f111855c4
Jon Lee <jage@fastmail.com>
2023-01-19 08:44:15 +0900

    Base URL update

--- config/urls.py
+++ config/urls.py
@@ -5,9 +5,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-    path('index/honyaku/archive/admin/', admin.site.urls),
-    path('index/honyaku/archive/accounts/', include('django.contrib.auth.urls')),
-    path('index/honyaku/archive/', include('resources.urls')),
+    path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', include('resources.urls')),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
