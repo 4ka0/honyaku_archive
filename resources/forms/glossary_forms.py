3317ec1d5f82b0f13199b79ea1c68a97d4cd26e3
Jon Lee <jage@fastmail.com>
2023-01-20 13:12:46 +0900

    Test update

--- resources/forms/glossary_forms.py
+++ resources/forms/glossary_forms.py
@@ -4,6 +4,42 @@ from django.core.validators import FileExtensionValidator
 from ..models import Glossary
 
 
+class GlossaryCreateForm(forms.ModelForm):
+    title = forms.CharField(
+        label='用語集のタイトル',
+        error_messages={
+            "required": "このフィールドは入力必須です。",
+        },
+    )
+    notes = forms.CharField(
+        label='備考（任意）',
+        required=False,
+        widget=forms.Textarea(attrs={'rows': 6}),
+    )
+
+    class Meta:
+        model = Glossary
+        fields = ('title', 'notes')
+
+
+class GlossaryUpdateForm(forms.ModelForm):
+    title = forms.CharField(
+        label='用語集のタイトル',
+        error_messages={
+            "required": "このフィールドは入力必須です。",
+        },
+    )
+    notes = forms.CharField(
+        label='備考（任意）',
+        widget=forms.Textarea(attrs={'rows': 6}),
+        required=False
+    )
+
+    class Meta:
+        model = Glossary
+        fields = ('title', 'notes')
+
+
 class GlossaryUploadForm(forms.ModelForm):
     glossary_file = forms.FileField(
         label="ファイルを選択してください。",
@@ -37,42 +73,6 @@ class GlossaryUploadForm(forms.ModelForm):
             if Glossary.objects.filter(title__iexact=title).exists():
                 msg = 'このタイトルの用語集はすでに存在しています。'
                 self.add_error('title', msg)
-
-
-class GlossaryCreateForm(forms.ModelForm):
-    title = forms.CharField(
-        label='用語集のタイトル',
-        error_messages={
-            "required": "このフィールドは入力必須です。",
-        },
-    )
-    notes = forms.CharField(
-        label='備考（任意）',
-        widget=forms.Textarea(attrs={'rows': 6}),
-        required=False
-    )
-
-    class Meta:
-        model = Glossary
-        fields = ('title', 'notes')
-
-
-class GlossaryUpdateForm(forms.ModelForm):
-    title = forms.CharField(
-        label='用語集のタイトル',
-        error_messages={
-            "required": "このフィールドは入力必須です。",
-        },
-    )
-    notes = forms.CharField(
-        label='備考（任意）',
-        widget=forms.Textarea(attrs={'rows': 6}),
-        required=False
-    )
-
-    class Meta:
-        model = Glossary
-        fields = ('title', 'notes')
 
 
 """
---
+++ resources/tests/test_forms/test_glossary_forms.py
@@ -0,0 +1,46 @@
+from django.test import TestCase
+from django.forms.widgets import Textarea
+
+from ...forms.glossary_forms import GlossaryCreateForm
+from ...models import Glossary
+
+# from freezegun import freeze_time
+
+# Test fields
+# - Labels, help text, and other properties you have set
+# Test form methods created yourself
+# Test Meta fields
+
+
+class TestGlossaryCreateForm(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        cls.form = GlossaryCreateForm()
+
+    # Test fields
+
+    def test_title_field_label(self):
+        self.assertEqual(self.form.fields['title'].label, '用語集のタイトル')
+
+    def test_title_field_required_error_message(self):
+        self.assertEqual(
+            self.form.fields['title'].error_messages['required'],
+            'このフィールドは入力必須です。'
+        )
+
+    def test_notes_field_label(self):
+        self.assertTrue(self.form.fields['notes'].label, '備考（任意）')
+
+    def test_notes_field_required(self):
+        self.assertFalse(self.form.fields['notes'].required)
+
+    def test_notes_field_widget(self):
+        self.assertTrue(self.form.fields['notes'].widget, Textarea)
+        self.assertEqual(self.form.fields['notes'].widget.attrs['rows'], 6)
+
+    def test_meta_model(self):
+        self.assertEqual(self.form._meta.model, Glossary)
+
+    def test_meta_fields_model(self):
+        self.assertEqual(self.form._meta.fields, ('title', 'notes'))
