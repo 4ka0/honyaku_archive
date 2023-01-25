1f8aca22342908052ac4cea7b62eda67c48a98c6
Jon Lee <jage@fastmail.com>
2023-01-25 12:44:45 +0900

    Test update

--- resources/tests/test_forms/test_glossary_forms.py
+++ resources/tests/test_forms/test_glossary_forms.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
-from django.forms.widgets import Textarea
+from django.forms.widgets import Textarea, TextInput
+from django.core.validators import FileExtensionValidator
+from django.contrib.auth import get_user_model
 
 from ...models import Glossary
 from ...forms.glossary_forms import GlossaryForm, GlossaryUploadForm
@@ -108,4 +110,108 @@ class TestGlossaryUploadForm(TestCase):
 
     @classmethod
     def setUpTestData(cls):
+        User = get_user_model()
+        cls.testuser = User.objects.create_user(
+            username="testuser",
+            email="testuser@email.com",
+            password="testuser123",
+        )
+        cls.glossary_obj = Glossary.objects.create(
+            title="test glossary",
+            notes="Test note.",
+            type="用語集",
+            created_by=cls.testuser,
+            updated_by=cls.testuser,
+        )
         cls.empty_form = GlossaryUploadForm()
+
+    # Test fields
+
+    def test_glossary_file_field_label(self):
+        self.assertEqual(self.empty_form.fields['glossary_file'].label, '① ファイルを選択してください。')
+
+    def test_glossary_file_field_required(self):
+        self.assertTrue(self.empty_form.fields["glossary_file"].required)
+
+    def test_glossary_file_field_required_error_message(self):
+        self.assertEqual(
+            self.empty_form.fields['glossary_file'].error_messages['required'],
+            'このフィールドは入力必須です。'
+        )
+
+    def test_glossary_file_field_validators_len(self):
+        self.assertEqual(len(self.empty_form.fields['glossary_file'].validators), 1)
+
+    def test_glossary_file_field_validator_type(self):
+        self.assertEqual(
+            type(self.empty_form.fields['glossary_file'].validators[0]),
+            FileExtensionValidator
+        )
+
+    def test_glossary_file_field_validator_allowed_extensions(self):
+        self.assertEqual(
+            self.empty_form.fields['glossary_file'].validators[0].allowed_extensions,
+            ["txt"]
+        )
+
+    def test_glossary_file_field_validators_allowed_extensions_error_message(self):
+        self.assertEqual(
+            self.empty_form.fields['glossary_file'].validators[0].message,
+            ['拡張子が".txt "のファイルを選択してください。']
+        )
+
+    def test_existing_glossary_field_label(self):
+        self.assertEqual(self.empty_form.fields['existing_glossary'].label, '② 既存の用語集に追加しますか？')
+
+    def test_existing_glossary_field_queryset(self):
+        expected = list(Glossary.objects.all().order_by('title'))
+        existing_glossary_queryset = list(self.empty_form.fields['existing_glossary'].queryset)
+        self.assertEqual(existing_glossary_queryset, expected)
+
+    def test_existing_glossary_field_required(self):
+        self.assertEqual(self.empty_form.fields['existing_glossary'].required, False)
+
+    def test_title_field_label(self):
+        self.assertEqual(self.empty_form.fields['title'].label, '③ または、新しい用語集を作成しますか？')
+
+    def test_title_field_widget(self):
+        self.assertTrue(self.empty_form.fields['title'].widget, TextInput)
+        self.assertEqual(
+            self.empty_form.fields['title'].widget.attrs['placeholder'],
+            '新しい用語集のタイトルを入力してください。'
+        )
+
+    def test_title_field_required(self):
+        self.assertEqual(self.empty_form.fields['title'].required, False)
+
+    def test_title_field_required_error_message(self):
+        self.assertEqual(
+            self.empty_form.fields['title'].error_messages['max_length'],
+            '100文字以下になるように変更してください。'
+        )
+
+    def test_notes_field_label(self):
+        self.assertEqual(self.empty_form.fields['notes'].label, '④ 備考（任意）')
+
+    def test_notes_field_widget(self):
+        self.assertTrue(self.empty_form.fields['notes'].widget, Textarea)
+        self.assertEqual(self.empty_form.fields['notes'].widget.attrs['rows'], 6)
+
+    def test_notes_field_required(self):
+        self.assertEqual(self.empty_form.fields['notes'].required, False)
+
+    def test_notes_field_help_text(self):
+        self.assertEqual(
+            self.empty_form.fields['notes'].help_text,
+            "アップロードされる用語集は既存の用語集に追加する場合、<br>上記の備考は既存の用語集の備考に追加されます。"
+        )
+
+    # Test Meta fields
+
+    def test_meta_model(self):
+        self.assertEqual(self.empty_form._meta.model, Glossary)
+
+    def test_meta_fields(self):
+        self.assertEqual(self.empty_form._meta.fields, ("glossary_file", "existing_glossary", "title", "notes"))
+
+    # Test form with input
