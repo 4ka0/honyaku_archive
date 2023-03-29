ffb141c40e5725a6220ebc9830696d93837f08aa
Jon Lee <jage@fastmail.com>
2023-03-29 09:50:09 +0900

    Segment form update

--- resources/forms/segment_forms.py
+++ resources/forms/segment_forms.py
@@ -6,12 +6,15 @@ from ..models import Segment
 class SegmentForm(forms.ModelForm):
     source = forms.CharField(
         label='① 原文',
+        widget=forms.Textarea(attrs={'rows': 6}),
         error_messages={
             "required": "このフィールドは入力必須です。",
         }
+
     )
     target = forms.CharField(
         label='② 訳文',
+        widget=forms.Textarea(attrs={'rows': 6}),
         error_messages={
             "required": "このフィールドは入力必須です。",
         }
