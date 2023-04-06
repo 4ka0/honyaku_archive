855798dc30bb55b2240bdf329c0ab67e3d2da91f
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-04-06 20:50:21 +0900

    App name change

--- archive/apps.py
+++ archive/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class ResourcesConfig(AppConfig):
+class ArchiveConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'resources'
+    name = 'archive'
