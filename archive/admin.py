71b9e41969212fc2dc50588ab1006bcc9ac0e6cb
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-07-22 10:42:39 +0900

    Resource and Item added to admin

--- archive/admin.py
+++ archive/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 from .models import (
-    Entry, Glossary, Translation, Segment
+    Entry, Glossary, Translation, Segment, Item, Resource
 )
 
 
@@ -52,7 +52,37 @@ class TranslationAdmin(admin.ModelAdmin):
     )
 
 
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'source',
+        'target',
+        'resource',
+    )
+    search_fields = ["source", "target"]
+
+
+class ResourceAdmin(admin.ModelAdmin):
+    fields = (
+        "resource_type",
+        "title",
+        "field",
+        "client",
+        "translator",
+        "notes",
+    )
+    list_display = (
+        "title",
+        "resource_type",
+        "field",
+        "client",
+        "translator",
+    )
+    search_fields = ["title"]
+
+
 admin.site.register(Entry, EntryAdmin)
 admin.site.register(Glossary, GlossaryAdmin)
 admin.site.register(Translation, TranslationAdmin)
 admin.site.register(Segment, SegmentAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Resource, ResourceAdmin)
