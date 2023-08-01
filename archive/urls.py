9b44975684b352141b25fd4fb60e1f656ad511aa
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-08-01 14:29:24 +0900

    URLs cleaned

--- archive/urls.py
+++ archive/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 
-from .views.glossary_views import (GlossaryCreateView,
-                                   GlossaryUpdateView, GlossaryUploadView)
+from .views.glossary_views import GlossaryCreateView, GlossaryUpdateView, GlossaryUploadView
 from .views.homepage_views import HomePageView, home_table_sort
 from .views.item_views import ItemCreateView, ItemDeleteView, ItemUpdateView
 from .views.resource_views import ResourceDeleteView, ResourceDetailView
@@ -14,19 +13,20 @@ urlpatterns = [
 
     path("search/", SearchView.as_view(), name="search"),
 
-    path("item/<int:pk>/edit/", ItemUpdateView.as_view(), name="update_item"),
-    path("item/<int:pk>/delete/", ItemDeleteView.as_view(), name="delete_item"),
-
-    path("item/new/", ItemCreateView.as_view(), name="create_item"),
+    path("resource/item/new/", ItemCreateView.as_view(), name="create_item"),
     path("resource/<int:resource>/additem/", ItemCreateView.as_view(), name="create_item"),
+    path("resource/item/<int:pk>/edit/", ItemUpdateView.as_view(), name="update_item"),
+    path("resource/item/<int:pk>/delete/", ItemDeleteView.as_view(), name="delete_item"),
 
     path("resource/<int:pk>/", ResourceDetailView.as_view(), name="resource_detail"),
     path("resource/<int:pk>/delete/", ResourceDeleteView.as_view(), name="resource_delete"),
 
-    path("glossary/new/", GlossaryCreateView.as_view(), name="glossary_create"),
-    path("glossary/upload/", GlossaryUploadView.as_view(), name="glossary_upload"),
-    path("glossary/<int:pk>/edit/", GlossaryUpdateView.as_view(), name="glossary_update"),
+    path("resource/glossary/new/", GlossaryCreateView.as_view(), name="glossary_create"),
+    path("resource/glossary/upload/", GlossaryUploadView.as_view(), name="glossary_upload"),
+    path("resource/glossary/<int:pk>/edit/", GlossaryUpdateView.as_view(), name="glossary_update"),
 
-    path("translation/upload/", TranslationUploadView.as_view(), name="translation_upload"),
-    path("translation/<int:pk>/edit/", TranslationUpdateView.as_view(), name="translation_update"),
+    path("resource/translation/upload/", TranslationUploadView.as_view(),
+         name="translation_upload"),
+    path("resource/translation/<int:pk>/edit/", TranslationUpdateView.as_view(),
+         name="translation_update"),
 ]
