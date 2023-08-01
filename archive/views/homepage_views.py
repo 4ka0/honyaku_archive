f78f91be954f233551fc0e54543501d9d9f37eb3
Jon Lee <39420293+4ka0@users.noreply.github.com>
2023-08-01 17:05:59 +0900

    Homepage code cleanup

--- archive/views/homepage_views.py
+++ archive/views/homepage_views.py
@@ -9,38 +9,15 @@ class HomePageView(LoginRequiredMixin, TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-        """
-        Create list of all resources ordered by creation date.
-        Also get the quantity of each type of resource.
-        """
-
-        """
-        # Could remove the following and just use the list of resources
-        # provided by the context processor. However, the order is different.
-        glossaries = Glossary.objects.all()
-        translations = Translation.objects.all()
-        resources_list = sorted(
-            chain(glossaries, translations),
-            key=lambda item: item.created_on,
-            reverse=True,
-        )
-        """
-
         resources_list = Resource.objects.all()
 
-        # num_of_glossaries = len(glossaries)
-        num_of_glossaries = Resource.objects.filter(resource_type="GLOSSARY").count()
-        # num_of_translations = len(translations)
-        num_of_translations = Resource.objects.filter(resource_type="TRANSLATION").count()
-        # num_of_resources = len(resources_list)
         num_of_resources = Resource.objects.all().count()
+        num_of_glossaries = Resource.objects.filter(resource_type="GLOSSARY").count()
+        num_of_translations = Resource.objects.filter(resource_type="TRANSLATION").count()
 
-        # num_of_gloss_entries = Entry.objects.all().count()
-        num_of_gloss_entries = Item.objects.filter(resource__resource_type="GLOSSARY").count()
-        # num_of_trans_segments = Segment.objects.all().count()
-        num_of_trans_segments = Item.objects.filter(resource__resource_type="TRANSLATION").count()
-        # num_of_all_entries = num_of_gloss_entries + num_of_trans_segments
         num_of_all_entries = Item.objects.all().count()
+        num_of_gloss_entries = Item.objects.filter(resource__resource_type="GLOSSARY").count()
+        num_of_trans_segments = Item.objects.filter(resource__resource_type="TRANSLATION").count()
 
         autofocus_searchbar = True
 
@@ -66,9 +43,6 @@ def home_table_sort(request, filter, direction):
     table shown on the homepage. Receives HTMX ajax calls from the template.
     """
 
-    # glossaries = Glossary.objects.all()
-    # translations = Translation.objects.all()
-    # resources = chain(glossaries, translations)
     resources = Resource.objects.all()
 
     if direction == "ascend":
